fcd4def7fea21c25b59bd8bce61f90d527ab66ea
Damian Czarnota <godevdamian@gmail.com>
2020-05-07 16:23:24 +0200

    hide-nav

--- js/navMobile.js
+++ js/navMobile.js
@@ -1,14 +1,30 @@
 const ham=document.querySelector('.hamburger1');
 const menu=document.querySelector('.navMobile');
+const navDesktop=document.querySelector(('.navDesktop'));
 
-let showMenu=()=>{
+ham.addEventListener('click', function(){
     menu.classList.toggle('navMobileActive');
-        
-}
-ham.addEventListener('click', showMenu);
+})
+
+window.addEventListener('click', function(event){
+    if(event.target != ham && event.target != menu){
+        menu.classList.remove('navMobileActive');
+    }
+})
 
 const menuItems=document.querySelector('.navMobile');
-
 menuItems.addEventListener('click', function(){
     menu.classList.remove('navMobileActive');
 })
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    ham.style.top = "0";
+    navDesktop.style.top = "0";
+  } else {
+   ham.style.top = "-50px";
+   navDesktop.style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
